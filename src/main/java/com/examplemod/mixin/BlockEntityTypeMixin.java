4fa2f2d1af6848eaf40d4c1784db50f12a1b0a1c
nyuppo <46286273+nyuppo@users.noreply.github.com>
2021-06-22 19:23:53 -0400

    Update BlockEntityTypeMixin.java

--- src/main/java/com/examplemod/mixin/BlockEntityTypeMixin.java
+++ src/main/java/com/examplemod/mixin/BlockEntityTypeMixin.java
@@ -1,8 +1,5 @@
 package com.examplemod.mixin;
 
-import com.examplemod.block.ModSignBlock;
-import com.examplemod.block.ModWallSignBlock;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,7 +12,7 @@ import net.minecraft.block.entity.BlockEntityType;
 public class BlockEntityTypeMixin {
     @Inject(method = "supports", at = @At("HEAD"), cancellable = true)
     private void supports(BlockState state, CallbackInfoReturnable<Boolean> info) {
-        if (BlockEntityType.SIGN.equals(this) && (state.getBlock() instanceof ModSignBlock || state.getBlock() instanceof ModWallSignBlock)) {
+        if (BlockEntityType.SIGN.equals(this) && (state.getBlock() instanceof SignBlock || state.getBlock() instanceof WallSignBlock)) {
             info.setReturnValue(true);
         }
     }
