ef127933567893606043db930775eaa28d701ce7
nyuppo <46286273+nyuppo@users.noreply.github.com>
2021-06-22 19:23:38 -0400

    Update ExampleMod.java

--- src/main/java/com/examplemod/ExampleMod.java
+++ src/main/java/com/examplemod/ExampleMod.java
@@ -1,15 +1,13 @@
 package com.examplemod;
 
-import com.examplemod.block.ModSignBlock;
-import com.examplemod.block.ModWallSignBlock;
 import com.examplemod.mixin.SignTypeAccessor;
 
 // left out all the default Minecraft imports here, but I'm sure you can figure those out lol
 
 public class ExampleMod implements ModInitializer {
     public static final SignType WILLOW_SIGN_TYPE = SignTypeAccessor.registerNew(SignTypeAccessor.newSignType("willow"));
-    public static final Block WILLOW_SIGN = new ModSignBlock(AbstractBlock.Settings.of(Material.WOOD, MapColor.PALE_GREEN).noCollision().strength(1.0F).sounds(BlockSoundGroup.WOOD), WILLOW_SIGN_TYPE);
-    public static final Block WILLOW_WALL_SIGN = new ModWallSignBlock(AbstractBlock.Settings.of(Material.WOOD, MapColor.PALE_GREEN).noCollision().strength(1.0F).sounds(BlockSoundGroup.WOOD).dropsLike(WILLOW_SIGN), WILLOW_SIGN_TYPE);
+    public static final Block WILLOW_SIGN = new SignBlock(AbstractBlock.Settings.of(Material.WOOD, MapColor.PALE_GREEN).noCollision().strength(1.0F).sounds(BlockSoundGroup.WOOD), WILLOW_SIGN_TYPE);
+    public static final Block WILLOW_WALL_SIGN = new WallSignBlock(AbstractBlock.Settings.of(Material.WOOD, MapColor.PALE_GREEN).noCollision().strength(1.0F).sounds(BlockSoundGroup.WOOD).dropsLike(WILLOW_SIGN), WILLOW_SIGN_TYPE);
 
     @Override
     public void onInitialize() {
