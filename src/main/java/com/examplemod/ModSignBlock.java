ad888b09569b6d260ee022a6b89cb47321525a6f
nyuppo <46286273+nyuppo@users.noreply.github.com>
2021-12-28 23:49:42 -0500

    Create ModSignBlock.java

---
+++ src/main/java/com/examplemod/ModSignBlock.java
@@ -0,0 +1,16 @@
+public class ModSignBlock extends SignBlock {
+
+    public ModSignBlock(Settings settings, SignType type) {
+        super(settings, type);
+    }
+
+    @Override
+    public final Identifier getLootTableId() {
+        Identifier correctedLootTableId = new Identifier(Caboodle.MOD_ID, "blocks/" + this.getSignType().getName() + "_sign");
+        if (this.lootTableId != correctedLootTableId) {
+            this.lootTableId = correctedLootTableId;
+        }
+
+        return this.lootTableId;
+    }
+}
